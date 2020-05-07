5c0d421eebeedebf9c22939c503686c46fa50ee5
Dmitry2489 <iva.klicunov2489@mail.ru>
2020-05-08 00:17:45 +0300

    +

--- app/js/main.js
+++ app/js/main.js
@@ -1,18 +1,13 @@
-import showAndHide from './modules/showAndHide';
+import showAndHide from "./modules/showAndHide";
 
-window.addEventListener('DOMContentLoaded', () => {
-    'use strict';
+window.addEventListener("DOMContentLoaded", () => {
+    "use strict";
     showAndHide(".header__btn-menu", ".block-user", "showBlock");
     showAndHide(".header__user", ".block-user", "showBlock");
     showAndHide(
-    ".projects__form-button",
-    ".categories-block",
-    "showBlock",
-    ".projects__filters"
-);
-
+        ".projects__form-button",
+        ".categories-block",
+        "showBlock",
+        ".projects__filters"
+    );
 });
-
-
-
-
