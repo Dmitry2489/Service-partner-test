3db568b77d0ae844d46d40917a7ccf37fa3a9c28
Dmitry2489 <iva.klicunov2489@mail.ru>
2020-05-08 00:16:19 +0300

    +

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,18 @@
-// import showAndHide from './modules/showAndHide';
+import showAndHide from './modules/showAndHide';
 
-// window.addEventListener('DOMContentLoaded', () => {
-//     'use strict';
-//     showAndHide();
-
-// });
-
-const showAndHide = (
-    selectorBtn = null,
-    selectorShow = null,
-    classActiveBLock = null,
-    selectorFilterItem = null
-) => {
-    let elementBtn = document.querySelector(selectorBtn),
-        elShow = document.querySelector(selectorShow),
-        elimentFilters = document.querySelector(selectorFilterItem);
-
-    elementBtn.addEventListener("click", (e) => {
-        if (e.target) {
-            e.preventDefault();
-        }
-        if (elimentFilters && window.screen.availWidth < 682) {
-            elimentFilters.classList.toggle(classActiveBLock);
-        }
-        if (elementBtn.classList.contains("header__btn-menu")) {
-            elementBtn.classList.toggle("activeMenu");
-        }
-
-        elShow.classList.toggle(classActiveBLock);
-    });
-};
-
-showAndHide(".header__btn-menu", ".block-user", "showBlock");
-showAndHide(".header__user", ".block-user", "showBlock");
-showAndHide(
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict';
+    showAndHide(".header__btn-menu", ".block-user", "showBlock");
+    showAndHide(".header__user", ".block-user", "showBlock");
+    showAndHide(
     ".projects__form-button",
     ".categories-block",
     "showBlock",
     ".projects__filters"
 );
+
+});
+
+
+
+
--- app/js/modules/showAndHide.js
+++ app/js/modules/showAndHide.js
@@ -1,7 +1,26 @@
-const showAndHide = (selector = null) => {
-    console.log('Ghkdvsgl')
-    let element = document.querySelector(selector);
- 
+const showAndHide = (
+    selectorBtn = null,
+    selectorShow = null,
+    classActiveBLock = null,
+    selectorFilterItem = null
+) => {
+    let elementBtn = document.querySelector(selectorBtn),
+        elShow = document.querySelector(selectorShow),
+        elimentFilters = document.querySelector(selectorFilterItem);
+
+    elementBtn.addEventListener("click", (e) => {
+        if (e.target) {
+            e.preventDefault();
+        }
+        if (elimentFilters && window.screen.availWidth < 682) {
+            elimentFilters.classList.toggle(classActiveBLock);
+        }
+        if (elementBtn.classList.contains("header__btn-menu")) {
+            elementBtn.classList.toggle("activeMenu");
+        }
+
+        elShow.classList.toggle(classActiveBLock);
+    });
 };
 
 export default showAndHide;
